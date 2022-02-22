315d732ddfc047e0ccb38467a7cfd8a3c34000db
Renatas? <renatasgal420@gmail.com>
2022-02-22 19:14:18 +0200

    🐣

---
+++ README.md
@@ -0,0 +1,3 @@
+## About
+
+Repository with up-to-date VILT stack (Laravel, Vue, Inertia, Tailwind) + Typescript
---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup name="DefaultLayout">
+import Home from '@/pages/Home.vue';
+</script>
+
+<template>
+  <div>
+    Default Layout
+    <Home />
+  </div>
+</template>
