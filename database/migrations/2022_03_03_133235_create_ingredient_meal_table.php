d5b7bb1515d7e0ecb011a724f1eb6522e56847ee
Renatas? <renatasgal420@gmail.com>
2022-03-03 21:27:45 +0200

    initial models and migrations

---
+++ database/migrations/2022_03_03_133235_create_ingredient_meal_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ingredient_meal', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('ingredient_id')
+                ->references('id')
+                ->on('ingredients')
+                ->onDelete('cascade');
+
+            $table->foreignId('meal_id')
+                ->references('id')
+                ->on('meals')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ingredient_meal');
+    }
+};
