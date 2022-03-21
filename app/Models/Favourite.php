111aaa81e2278e98f4f9abf0c02a9bfdeedf12de
Renatas? <renatasgal420@gmail.com>
2022-03-21 14:37:48 +0200

    Favourite and User relationship

--- app/Models/Favourite.php
+++ app/Models/Favourite.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class Favourite extends Model
 {
     use HasFactory;
+
+    public function breakfast()
+    {
+        return $this->belongsTo(Meal::class);
+    }
+
+    public function lunch()
+    {
+        return $this->belongsTo(Meal::class);
+    }
+
+    public function snack()
+    {
+        return $this->belongsTo(Meal::class);
+    }
+
+    public function dinner()
+    {
+        return $this->belongsTo(Meal::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
