d87c27fc36108bd006c0e0fb529908d66842ca72
Renatas? <renatasgal420@gmail.com>
2022-03-25 22:19:39 +0200

    add BaseIcon

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup name="BaseIcon">
+const { icon } = defineProps<{
+  icon: string;
+}>();
+
+const iconPath = computed(
+  // cache query is used for busting cache ?cache=1.2.2
+  () => `/dashboard.svg#${icon}`,
+);
+</script>
+
+<template>
+  <svg class="flex-shrink-0 block fill-current">
+    <use :href="iconPath" :xlink:href="iconPath" />
+  </svg>
+</template>
