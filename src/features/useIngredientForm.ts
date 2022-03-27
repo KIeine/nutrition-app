6f79cd041a5793a98a848fed0e46687eb1a626b5
Renatas? <renatasgal420@gmail.com>
2022-03-28 00:40:30 +0300

    add ingredients form and schema

---
+++ src/features/useIngredientForm.ts
@@ -0,0 +1,74 @@
+import { useForm } from '@inertiajs/inertia-vue3';
+
+export const useIngredientForm = () => {
+  const form = useForm({
+    name: null,
+    description: null,
+    carbohydrates: null,
+    protein: null,
+    fat: null,
+    sugar: null,
+    fiber: null,
+    calories: null,
+    image: null,
+  });
+
+  const schema = computed(() => ({
+    name: {
+      name: 'name',
+      title: 'Food name',
+      type: 'text',
+    },
+    description: {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    },
+    carbohydrates: {
+      name: 'carbohydrates',
+      title: 'Carbohydrates',
+      type: 'number',
+      min: 0,
+    },
+    protein: {
+      name: 'protein',
+      title: 'Protein',
+      type: 'number',
+      min: 0,
+    },
+    fat: {
+      name: 'fat',
+      title: 'Fat',
+      type: 'number',
+      min: 0,
+    },
+    sugar: {
+      name: 'sugar',
+      title: 'Sugar',
+      type: 'number',
+      min: 0,
+    },
+    fiber: {
+      name: 'fiber',
+      title: 'Fiber',
+      type: 'number',
+      min: 0,
+    },
+    calories: {
+      name: 'calories',
+      title: 'Calories',
+      type: 'number',
+      min: 0,
+    },
+    image: {
+      name: 'image',
+      title: 'Image',
+      type: 'file',
+    },
+  }));
+
+  return {
+    form,
+    schema,
+  };
+};
