f749414a9c64bb771428c924c271c438b51ec749
Renatas? <renatasgal420@gmail.com>
2022-03-27 19:10:36 +0300

    add ingredient routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\IngredientController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,4 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', HomeController::class);
+Route::get('/', fn () => to_route('home'));
+
+Route::get('/home', HomeController::class)->name('home');
+
+Route::controller(IngredientController::class)->group(function () {
+    Route::get('/ingredients', 'index')->name('ingredients.index');
+    Route::post('/ingredients', 'store')->name('ingredients.store');
+});
