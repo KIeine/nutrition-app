01f688a39c37d43ec2d28f3f844b4bfa5ce8ea80
Renatas? <renatasgal420@gmail.com>
2022-03-27 23:35:51 +0300

    base button: add props validation with ts

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,9 +1,25 @@
-<script setup lang="ts" name="BaseButton"></script>
+<script setup lang="ts" name="BaseButton">
+interface types {
+  primary: string;
+  'primary-inverse': string;
+}
+
+const { type = 'primary' } = defineProps<{
+  type?: keyof types;
+}>();
+
+const TYPE_TO_CLASS: types = {
+  primary:
+    'text-white bg-pink-500 rounded-md hover:bg-pink-500/70 focus:bg-pink-500',
+  'primary-inverse':
+    'text-pink-500 bg-white rounded-md border border-pink-500 hover:bg-pink-500/20 focus:bg-pink-500/10',
+};
+
+const classesByType = computed(() => TYPE_TO_CLASS[type]);
+</script>
 
 <template>
-  <button
-    class="px-4 py-2 text-white bg-pink-500 rounded-md hover:bg-pink-500/70 focus:bg-pink-500 focus:shadow-lg"
-  >
+  <button class="px-4 py-2 focus:shadow-lg" :class="classesByType">
     <slot />
   </button>
 </template>
