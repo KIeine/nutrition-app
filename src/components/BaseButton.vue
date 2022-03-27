2183a7354a45c4e839d90c9997830e454375c1ca
Renatas? <renatasgal420@gmail.com>
2022-03-27 19:09:39 +0300

    add base button component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts" name="BaseButton"></script>
+
+<template>
+  <button
+    class="px-4 py-2 text-white bg-pink-500 rounded-md hover:bg-pink-500/70 focus:bg-pink-500 focus:shadow-lg"
+  >
+    <slot />
+  </button>
+</template>
