9a47dc6e18d6530220e9a07a98bbd47656495d3d
Renatas? <renatasgal420@gmail.com>
2022-03-27 19:10:02 +0300

    increase default layout margin

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,11 +4,6 @@ namespace App\Http\Controllers;
 
 class HomeController extends Controller
 {
-    /**
-     * Provision a new web server.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function __invoke()
     {
         return inertia('Home');
--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -6,7 +6,7 @@ import Navbar from '@/components/Navbar.vue';
 <template>
   <div class="flex flex-col w-full min-h-screen">
     <Navbar />
-    <main class="px-5 pt-24 mb-20 md:px-40">
+    <main class="px-5 mt-32 mb-20 md:px-40">
       <div class="max-w-4xl mx-auto desktop:max-w-7xl">
         <slot />
       </div>
