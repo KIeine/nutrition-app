8a06ba53daf6eabf24113d13faa9acad359d7ed9
Renatas? <renatasgal420@gmail.com>
2022-03-27 19:36:06 +0300

    add more columns to ingredients table

---
+++ database/migrations/2022_03_27_163323_add_fiber_and_sugar_columns_to_ingredients_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('ingredients', function (Blueprint $table) {
+            $table->float('sugar')->nullable()->after('carbohydrates');
+            $table->float('fiber')->nullable()->after('fat');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('ingredients', function (Blueprint $table) {
+            $table->dropColumn('sugar');
+            $table->dropColumn('fiber');
+        });
+    }
+};
