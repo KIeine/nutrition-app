1250c9061757afd4a6eae6a8625030dbf0a7f322
Renatas? <renatasgal420@gmail.com>
2022-03-28 19:16:55 +0300

    add errors and emit file from file field

--- src/components/FormFileField.vue
+++ src/components/FormFileField.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts" name="FormFileField">
+import { PROVIDE_ID_TO_KEY } from '@/features/useProvideInjectKeys';
+
 interface Props {
   name: string;
   title: string;
   accept: string;
   required?: boolean;
-  modelValue?: string | null;
+  modelValue?: File | null;
 }
 
 const emit = defineEmits<{
-  (e: 'update:modelValue', value: string): void;
+  (e: 'update:modelValue', value: File): void;
 }>();
 
 const {
@@ -19,8 +21,16 @@ const {
   modelValue = '',
 } = defineProps<Props>();
 
+const injectKey = PROVIDE_ID_TO_KEY.ingredientsCreate;
+const formErrors = inject<{
+  [key: string]: string;
+}>(injectKey);
+
 const onInput = (e: Event) => {
-  emit('update:modelValue', (e.target as HTMLInputElement).value);
+  emit(
+    'update:modelValue',
+    ((e.target as HTMLInputElement).files as FileList)[0],
+  );
 };
 </script>
 
@@ -31,13 +41,18 @@ const onInput = (e: Event) => {
       <span v-if="required" class="text-red-500">*</span>
     </span>
 
-    <input
-      :id="name"
-      type="file"
-      :accept="accept"
-      :value="modelValue"
-      @input="onInput"
-      class="w-full p-1 border rounded-md outline-none md:w-96 hover:shadow-input-hover focus:shadow-input-focus"
-    />
+    <div>
+      <input
+        :id="name"
+        type="file"
+        :accept="accept"
+        :value="modelValue"
+        @input="onInput"
+        class="w-full p-1 border rounded-md outline-none md:w-96 hover:shadow-input-hover focus:shadow-input-focus"
+      />
+      <p v-if="formErrors?.[name]" class="mt-1 text-xs text-red-500">
+        {{ formErrors[name] }}
+      </p>
+    </div>
   </label>
 </template>
