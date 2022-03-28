79b4b9947c79288c553933112e9d5c9b3f3d0855
Renatas? <renatasgal420@gmail.com>
2022-03-28 15:47:34 +0300

    rename text field

---
+++ src/components/FormTextField.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts" name="FormTextField">
+interface Props {
+  name: string;
+  type: string;
+  title: string;
+  small?: boolean;
+  min?: number;
+  required?: boolean;
+  modelValue?: string | null;
+}
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: string | number | null): void;
+}>();
+
+const {
+  name,
+  type,
+  title,
+  small = false,
+  min = 0,
+  required = false,
+  modelValue = '',
+} = defineProps<Props>();
+
+const onInput = (e: Event) => {
+  emit('update:modelValue', (e.target as HTMLInputElement).value);
+};
+</script>
+
+<template>
+  <label :for="name" class="flex flex-col items-center sm:flex-row">
+    <span class="min-w-[120px] md:mr-20">
+      {{ title }}
+      <span v-if="required" class="text-red-500">*</span>
+    </span>
+
+    <input
+      :id="name"
+      :min="min"
+      :type="type"
+      :required="required"
+      :value="modelValue"
+      @input="onInput"
+      :class="{ 'w-32': small, 'w-full md:w-96': !small }"
+      class="p-1 border rounded-md outline-none hover:shadow-input-hover focus:shadow-input-focus"
+    />
+  </label>
+</template>
