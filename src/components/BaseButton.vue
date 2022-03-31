54bed37edc1b38122172cd013bdbdc5b944fa0e4
Renatas? <renatasgal420@gmail.com>
2022-03-31 20:28:32 +0300

    add states to base button

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,25 +1,40 @@
 <script setup lang="ts" name="BaseButton">
-interface types {
+interface theme {
   primary: string;
   'primary-inverse': string;
 }
 
-const { type = 'primary' } = defineProps<{
-  type?: keyof types;
+const {
+  theme = 'primary',
+  disabled = false,
+  loading = false,
+} = defineProps<{
+  theme?: keyof theme;
+  disabled?: boolean;
+  loading?: boolean;
 }>();
 
-const TYPE_TO_CLASS: types = {
+const THEME_TO_CLASS = $computed(() => ({
   primary:
-    'text-white bg-pink-500 rounded-md hover:bg-pink-500/70 focus:bg-pink-500',
-  'primary-inverse':
-    'text-pink-500 bg-white rounded-md border border-pink-500 hover:bg-pink-500/20 focus:bg-pink-500/10',
-};
+    disabled || loading
+      ? 'bg-gray-200 text-white'
+      : 'text-white bg-pink-500 hover:bg-pink-500/70 focus:bg-pink-500',
 
-const classesByType = computed(() => TYPE_TO_CLASS[type]);
+  'primary-inverse':
+    disabled || loading
+      ? 'border border-gray-200 text-gray-200'
+      : 'text-pink-500 bg-white border border-pink-500 hover:bg-pink-500/20 focus:bg-pink-500/10',
+}));
+
+const classesByTheme = computed(() => THEME_TO_CLASS[theme]);
 </script>
 
 <template>
-  <button class="px-4 py-2 focus:shadow-lg" :class="classesByType">
+  <button
+    class="px-4 py-2 rounded-md focus:shadow-lg"
+    :class="classesByTheme"
+    :disabled="loading || disabled"
+  >
     <slot />
   </button>
 </template>
