ff45317792c50f5d1873f8fbab2104e9b3ac24a9
Renatas? <renatasgal420@gmail.com>
2022-03-31 18:40:49 +0300

    fix file field errors

--- src/components/FormFileUpload.vue
+++ src/components/FormFileUpload.vue
@@ -1,30 +1,23 @@
-<script setup lang="ts" name="FormFileField">
+<script setup lang="ts" name="FormFileUpload">
 import { PROVIDE_ID_TO_KEY } from '@/features/useProvideInjectKeys';
+
+const injectKey = PROVIDE_ID_TO_KEY.ingredientsCreate;
+const formErrors = inject<{
+  [key: string]: string;
+}>(injectKey);
 
 interface Props {
   name: string;
   title: string;
   accept: string;
   required?: boolean;
-  modelValue?: File | null;
 }
+
+const { name, title, accept, required = false } = defineProps<Props>();
 
 const emit = defineEmits<{
   (e: 'update:modelValue', value: File): void;
 }>();
-
-const {
-  name,
-  title,
-  accept,
-  required = false,
-  modelValue = '',
-} = defineProps<Props>();
-
-const injectKey = PROVIDE_ID_TO_KEY.ingredientsCreate;
-const formErrors = inject<{
-  [key: string]: string;
-}>(injectKey);
 
 const onInput = (e: Event) => {
   emit(
@@ -43,12 +36,11 @@ const onInput = (e: Event) => {
 
     <div>
       <input
-        :id="name"
         type="file"
+        :id="name"
         :accept="accept"
-        :value="modelValue"
-        @input="onInput"
         class="w-full p-1 border rounded-md outline-none md:w-96 hover:shadow-input-hover focus:shadow-input-focus"
+        @change="onInput"
       />
       <p v-if="formErrors?.[name]" class="mt-1 text-xs text-red-500">
         {{ formErrors[name] }}
