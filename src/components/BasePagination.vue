06a1ee8435aaac0833a6e44482ac65c3fd1d4a9a
Renatas? <renatasgal420@gmail.com>
2022-04-16 13:07:56 +0300

    add base pagination component

---
+++ src/components/BasePagination.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts" name="BasePagination">
+import { paginationLink } from '@/features/useTypes';
+
+const { links } = defineProps<{
+  links: paginationLink[];
+}>();
+</script>
+
+<template>
+  <div class="flex space-x-4">
+    <component
+      v-for="link in links"
+      :key="link.label"
+      :is="link.url ? 'InertiaLink' : 'span'"
+      :href="link.url"
+      v-html="link.label"
+      :class="{
+        'text-gray-500': !link.url,
+        'hover:text-pink-500': link.url,
+        'text-pink-500': link.active,
+      }"
+    />
+  </div>
+</template>
