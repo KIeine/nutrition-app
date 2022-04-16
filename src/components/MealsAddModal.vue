aa55007b9bcf1f07f9ff24e004d5187beeca9134
Renatas? <renatasgal420@gmail.com>
2022-04-16 07:39:37 +0300

    move meals ingredients to another component

--- src/components/MealsAddModal.vue
+++ src/components/MealsAddModal.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts" name="MealsAddModal">
 import { useMealsForm } from '@/features/useMealsForm';
-import { Ingredient } from '@/features/useTypes';
 
 import FormModal from './FormModal.vue';
 import FormTextField from './FormTextField.vue';
@@ -8,6 +7,7 @@ import FormFileUpload from './FormFileUpload.vue';
 import FormTextareaField from './FormTextareaField.vue';
 import FormSelectField from './FormSelectField.vue';
 import FormIngredientsField from './FormIngredientsField.vue';
+import MealsSelectedIngredients from './MealsSelectedIngredients.vue';
 
 const emit = defineEmits<{
   (e: 'close'): void;
@@ -32,8 +32,8 @@ const onSubmit = () => {
     });
 };
 
-const onRemoveIngredient = (ingredient: Ingredient) => {
-  form.ingredients = form.ingredients.filter((x) => x.id !== ingredient.id);
+const onRemoveIngredient = (id: number) => {
+  form.ingredients = form.ingredients.filter((x) => x.id !== id);
 };
 </script>
 
@@ -48,44 +48,10 @@ const onRemoveIngredient = (ingredient: Ingredient) => {
       v-model="form.ingredients"
     />
 
-    <div v-if="form.ingredients.length">
-      <p class="mb-2">Selected ingredients:</p>
-      <div
-        v-for="ingredient in form.ingredients"
-        :key="ingredient.id"
-        class="flex items-center p-1 rounded-md group hover:bg-gray-100 hover:cursor-pointer"
-        @click.stop="onRemoveIngredient(ingredient)"
-      >
-        <img :src="ingredient.image" alt="" class="w-16 h-16 rounded-lg" />
-
-        <div class="flex flex-col ml-2 space-y-1">
-          <span class="w-32">{{ ingredient.name }}</span>
-          <input
-            @click.stop
-            placeholder="Notes"
-            v-model="form.notes[ingredient.id]"
-            class="px-1 border rounded-md outline-none w-52 hover:shadow-input-hover focus:shadow-input-focus"
-          />
-        </div>
-
-        <div class="flex flex-col ml-2 space-y-1">
-          <label for="quantity">Serving quantity</label>
-          <input
-            id="quantity"
-            @click.stop
-            type="number"
-            min="0"
-            placeholder="1"
-            v-model="form.quantities[ingredient.id]"
-            class="px-1 border rounded-md outline-none w-14 hover:shadow-input-hover focus:shadow-input-focus"
-          />
-        </div>
-
-        <BaseIcon
-          icon="close"
-          class="hidden w-4 h-4 ml-auto mr-2 text-gray-400 group-hover:block"
-        />
-      </div>
-    </div>
+    <MealsSelectedIngredients
+      v-if="form.ingredients.length"
+      :form="form"
+      @remove="onRemoveIngredient"
+    />
   </FormModal>
 </template>
---
+++ src/components/MealsSelectedIngredients.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts" name="MealsSelectedIngredients">
+import { Ingredient } from '@/features/useTypes';
+
+const emit = defineEmits<{
+  (e: 'remove', value: number): void;
+}>();
+
+const { form } = defineProps<{
+  form: {
+    ingredients: Ingredient[];
+    notes: {
+      [key: string]: string;
+    };
+    quantities: {
+      [key: string]: string;
+    };
+  };
+}>();
+
+const onRemove = (id: number) => {
+  emit('remove', id);
+};
+</script>
+
+<template>
+  <div>
+    <p class="mb-2">Selected ingredients:</p>
+    <div
+      v-for="ingredient in form.ingredients"
+      :key="ingredient.id"
+      class="flex items-center p-1 rounded-md group hover:bg-gray-100 hover:cursor-pointer"
+      @click.stop="onRemove(ingredient.id)"
+    >
+      <img :src="ingredient.image" alt="" class="w-16 h-16 rounded-lg" />
+
+      <div class="flex flex-col ml-2 space-y-1">
+        <span class="w-32">{{ ingredient.name }}</span>
+        <input
+          @click.stop
+          placeholder="Notes"
+          v-model="form.notes[ingredient.id]"
+          class="px-1 border rounded-md outline-none w-52 hover:shadow-input-hover focus:shadow-input-focus"
+        />
+      </div>
+
+      <div class="flex flex-col ml-2 space-y-1">
+        <label for="quantity">Serving quantity</label>
+        <input
+          id="quantity"
+          @click.stop
+          type="number"
+          min="0"
+          placeholder="1"
+          v-model="form.quantities[ingredient.id]"
+          class="px-1 border rounded-md outline-none w-14 hover:shadow-input-hover focus:shadow-input-focus"
+        />
+      </div>
+
+      <BaseIcon
+        icon="close"
+        class="hidden w-4 h-4 ml-auto mr-2 text-gray-400 group-hover:block"
+      />
+    </div>
+  </div>
+</template>
