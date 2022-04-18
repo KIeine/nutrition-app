af3533c5808ba2b76fad5cd2d751fab4e3a552f9
Renatas? <renatasgal420@gmail.com>
2022-04-18 11:54:49 +0300

    add nutrient groups component

---
+++ src/components/NutrientGroupsTotals.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts" name="NutrientGroupsTotals">
+const { nutritionalItems } = defineProps<{
+  nutritionalItems: {
+    label: string;
+    value: string | number;
+    postfix?: string;
+  }[];
+}>();
+</script>
+
+<template>
+  <div class="text-sm font-light">
+    <div
+      v-for="item in nutritionalItems"
+      :key="item.label"
+      class="flex items-center justify-between"
+    >
+      <p>{{ item.label }}:</p>
+      <p>{{ item.value }}{{ item.postfix }}</p>
+    </div>
+  </div>
+</template>
