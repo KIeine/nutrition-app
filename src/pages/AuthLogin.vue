3444f87f769aa07a6ae4f44dc734e07905dd72e7
Renatas? <renatasgal420@gmail.com>
2022-04-18 16:11:12 +0300

    add register link in login

--- src/pages/AuthLogin.vue
+++ src/pages/AuthLogin.vue
@@ -19,6 +19,7 @@ const onSubmit = () => {
 
 <template>
   <main class="flex items-center justify-center min-h-screen">
+    <InertiaHead title="Login" />
     <section class="flex flex-col p-5 rounded-md shadow-md">
       <h2 class="text-xl font-semibold">Login</h2>
 
@@ -26,6 +27,13 @@ const onSubmit = () => {
         <FormTextField v-model="form.email" v-bind="schema.email" />
         <FormTextField v-model="form.password" v-bind="schema.password" />
       </BaseForm>
+
+      <p class="text-sm">
+        Don't have an account yet?
+        <InertiaLink class="text-pink-500" href="/register">
+          Register here.
+        </InertiaLink>
+      </p>
     </section>
   </main>
 </template>
