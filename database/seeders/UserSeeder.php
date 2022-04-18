db685490853836185d8719ab15f3c3985dd1166b
Renatas? <renatasgal420@gmail.com>
2022-04-18 15:16:30 +0300

    add user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use DB;
+use Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email' => 'test@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
