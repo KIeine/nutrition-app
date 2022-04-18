a316593ef3ecb999b1b42a99076a13d7eea31695
Renatas? <renatasgal420@gmail.com>
2022-04-18 15:17:58 +0300

    add base form

---
+++ src/components/BaseForm.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts" name="BaseForm">
+const emit = defineEmits<{
+  (e: 'submit'): void;
+}>();
+
+const { isDisabled = false, isLoading = false } = defineProps<{
+  isDisabled?: boolean;
+  isLoading?: boolean;
+}>();
+
+const onSubmit = () => {
+  emit('submit');
+};
+</script>
+
+<template>
+  <form @submit.prevent="onSubmit">
+    <div class="space-y-4 sm:px-10">
+      <slot />
+    </div>
+
+    <div class="my-10 text-sm text-gray-500">
+      <span class="text-red-500">*</span> marked fields are required.
+    </div>
+
+    <div class="flex justify-end space-x-4">
+      <BaseButton :disabled="isDisabled" :loading="isLoading" class="w-24">
+        Submit
+      </BaseButton>
+    </div>
+  </form>
+</template>
