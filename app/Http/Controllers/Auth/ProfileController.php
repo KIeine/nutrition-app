73009e10b44a4d30c864a346b1842f2399938d00
Renatas? <renatasgal420@gmail.com>
2022-05-04 15:13:59 +0300

    faeture: add pagination to profile favourites

--- app/Http/Controllers/Auth/ProfileController.php
+++ app/Http/Controllers/Auth/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Favourite;
 use App\Models\Meal;
 use Illuminate\Http\Request;
 
@@ -10,20 +11,23 @@ class ProfileController extends Controller
 {
     public function show()
     {
-        $favourites = auth()->user()->favourites->sortByDesc('created_at')->values()->map(function ($fav) {
-            $breakfast = Meal::find($fav->breakfast_id);
-            $lunch = Meal::find($fav->lunch_id);
-            $dinner = Meal::find($fav->dinner_id);
+        $favourites = Favourite::where('user_id', auth()->user()->id)
+            ->orderBy('created_at', 'desc')
+            ->paginate(5)
+            ->through(function ($fav) {
+                $breakfast = Meal::find($fav->breakfast_id);
+                $lunch = Meal::find($fav->lunch_id);
+                $dinner = Meal::find($fav->dinner_id);
 
-            return [
-                'id' => $fav->id,
-                'breakfast' => $breakfast,
-                'lunch' => $lunch,
-                'dinner' => $dinner,
-                'calories' => $this->countCalories($breakfast, $lunch, $dinner),
-                'created_at' => date_format($fav->created_at, 'd M \'y')
-            ];
-        });
+                return [
+                    'id' => $fav->id,
+                    'breakfast' => $breakfast,
+                    'lunch' => $lunch,
+                    'dinner' => $dinner,
+                    'calories' => $this->countCalories($breakfast, $lunch, $dinner),
+                    'created_at' => date_format($fav->created_at, 'd M \'y')
+                ];
+            });
 
         return inertia('Profile', ['favourites' => $favourites]);
     }
--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,18 +1,21 @@
 <script setup lang="ts" name="Profile">
 import { Inertia } from '@inertiajs/inertia';
-import { Meal } from '@/features/useTypes';
+import { Meal, laravelPagination } from '@/features/useTypes';
 
 import FavouritePlanItem from '@/components/FavouritePlanItem.vue';
+import BasePagination from '@/components/BasePagination.vue';
 
-defineProps<{
-  favourites: {
-    id: number;
-    calories: number;
-    breakfast: Meal;
-    lunch: Meal;
-    dinner: Meal;
-    created_at: string;
-  }[];
+const { favourites } = defineProps<{
+  favourites: laravelPagination & {
+    data: {
+      id: number;
+      calories: number;
+      breakfast: Meal;
+      lunch: Meal;
+      dinner: Meal;
+      created_at: string;
+    }[];
+  };
 }>();
 
 const onRemove = (id: number) => {
@@ -28,15 +31,23 @@ const onRemove = (id: number) => {
 
     <h5 class="mt-20 text-xl">Saved meal plans</h5>
 
-    <div class="grid grid-cols-3 gap-6 mt-10 desktop:grid-cols-4">
+    <div
+      v-if="favourites.data.length"
+      class="grid grid-cols-3 gap-6 mt-10 desktop:grid-cols-4"
+    >
       <FavouritePlanItem
-        v-for="item in favourites"
+        v-for="item in favourites.data"
         :key="item.id"
         :item="item"
         @remove="onRemove"
       />
-
-      <p v-if="!favourites.length">No meal plans saved yet.</p>
     </div>
+    <p v-else>No meal plans saved yet.</p>
+
+    <BasePagination
+      v-if="favourites.last_page > 1"
+      :links="favourites.links"
+      class="mt-10"
+    />
   </div>
 </template>
